5db5758ea99bfa8c6e33750aaf2caa3dc7357687
Flavio Meneses <flameneses04@gmail.com>
2026-05-02 18:49:26 -0300

    Criada a Classe Seller

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,12 +1,17 @@
 package Application;
 
 import Model.Entities.Department;
+import Model.Entities.Seller;
+import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "books");
 
-        System.out.println(obj);
+        LocalDate birthDate = LocalDate.of(2004, 12, 11);
+        Seller seller =  new Seller(1, "Flavio", "flavio@gmail.com", birthDate, 1620.0, obj);
+
+        System.out.println(seller);
     }
 }
