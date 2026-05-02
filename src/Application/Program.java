541733bafc1acf9f11af7e145beabd3633dc28b1
Flavio Meneses <flameneses04@gmail.com>
2026-05-02 18:36:14 -0300

    Criada classe Department

---
+++ src/Application/Program.java
@@ -0,0 +1,12 @@
+package Application;
+
+import Model.Entities.Department;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "books");
+
+        System.out.println(obj);
+    }
+}
